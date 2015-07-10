d87b86e2feb8fd5022649df95743eef88ab7a4df
onea <oneassasin@rambler.ru>
2015-07-10 21:49:28 +0300

    + userRolesController: getUserRoles

--- controllers/userRolesController.js
+++ controllers/userRolesController.js
@@ -2,6 +2,21 @@ const squel = require('squel');
 const escaper = require('../libs/escaper');
 
 module.exports.getUserRoles = function(req, res, next) {
+  const skipInt = parseInt(req.sanitize('skip').escape() || 0);
+  var countInt = parseInt(req.sanitize('count').escape() || 25);
+  if (countInt > 25)
+    countInt = 25;
+  const query = squel.select()
+    .from('juvity.user_roles')
+    .limit(countInt)
+    .offset(skipInt)
+    .toString();
+  req.pgClient.promiseQuery(query)
+    .then(function(results) {
+      res.status(200).send(results.rows).end();
+    })
+    .fail(next)
+    .fin(req.closeClient);
 };
 
 module.exports.createUserRoleId = function(req, res, next) {
